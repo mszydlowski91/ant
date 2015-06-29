523d8d6c6efe31bbc6fc296def3d515c38ffde33
Michał <michal.k.szydlowski@gmail.com>
2015-06-29 22:41:24 +0200

    z powrotem eclipse

---
+++ src/com/example/scrumboard/model/MemberFolder.java
@@ -0,0 +1,22 @@
+package com.example.scrumboard.model;
+
+public class MemberFolder {
+
+	private int memberId;
+	private int folderId;
+	
+	
+	public int getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(int memberId) {
+		this.memberId = memberId;
+	}
+	public int getFolderId() {
+		return folderId;
+	}
+	public void setFolderId(int folderId) {
+		this.folderId = folderId;
+	}
+	
+}
